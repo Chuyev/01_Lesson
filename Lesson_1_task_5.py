b84a13d51035fb775259280b562b9c600d101b62
Chuyev <chuyev_sergei@mail.ru>
2025-06-19 19:09:34 +0300

    Add files via upload

---
+++ Lesson_1_task_5.py
@@ -0,0 +1,18 @@
+def print_num(num):
+    print(num, end = "")
+   
+print_num('8')
+print_num('8')
+print_num('0')
+print_num('0')
+print_num('5')
+print_num('5')
+print_num('5')
+print_num('3')
+print_num('5')
+print_num('3')
+print_num('5')
+
+# По условию мне необходимо выполнить: Вызовите эту функцию 11 раз так, чтобы в консоль вывелся номер
+# 88005553535, где здесь "Лишние вызовы print_num('5') и print_num('3') (номер содержит только
+# одну 5 и 3 в конце)".
